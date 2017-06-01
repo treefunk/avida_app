d7e796b5ad19ece7b954271444f04d2ed32d19e4
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-06-01 18:49:04 +0800

    update crud test other methods

--- application/tests/controllers/api/Crud_test.php
+++ application/tests/controllers/api/Crud_test.php
@@ -10,8 +10,18 @@ class Crud_test extends TestCase
     $this->assertResponseCode(200);
     $this->assertContains($expected, $output);
 
-    $output = $this->request('Post', ['Crud', 'index_post']);
+    $output = $this->request('POST', ['Crud', 'index_post']);
     $expected = 'Post request!';
+    $this->assertResponseCode(200);
+    $this->assertContains($expected, $output);
+
+    $output = $this->request('PUT', ['Crud', 'index_put']);
+    $expected = 'Put request!';
+    $this->assertResponseCode(200);
+    $this->assertContains($expected, $output);
+
+    $output = $this->request('PATCH', ['Crud', 'index_patch']);
+    $expected = 'Patch request!';
     $this->assertResponseCode(200);
     $this->assertContains($expected, $output);
 
