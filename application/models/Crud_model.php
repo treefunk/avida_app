dd7664726ca3e6ce239918008dcba180e49864c5
Lorenzo Salamante <enzo@webcraftph.com>
2017-06-04 13:59:28 +0800

    add all(), get(), and add() to crud model

--- application/models/Crud_model.php
+++ application/models/Crud_model.php
@@ -3,15 +3,31 @@
 class Crud_model extends CI_model
 {
 
+  # Your table name here
+  protected $table = 'crud';
+
   public function __construct()
   {
     parent::__construct();
 
   }
 
-  public function getUsers()
+  public function all()
   {
-    return $this->db->get('users')->result();
+    return $this->db->get($this->table)->result();
+  }
+
+  public function get($id)
+  {
+    $this->db->where('id', $id);
+    return $this->db->get($this->table)->result();
+  }
+
+  public function add($data)
+  {
+    if($this->db->insert($this->table, $data)){
+      return $this->db->insert_id();
+    }
   }
 
 }
