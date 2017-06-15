ede2d00c6c9f332f027aa104727e08671420a888
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-06-15 15:44:36 +0800

    modify readme

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -7,7 +7,7 @@ class Migrate extends CI_Controller
   {
     $this->load->library('migration');
 
-    switch(@$mode){
+    switch($mode){
       case 'reset':
       if ($this->migration->current() === FALSE){
         show_error($this->migration->error_string());
