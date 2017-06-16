4e950f3b7ec6e563b81744be53f4d80438cf4aea
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-06-16 17:20:39 +0800

    add edit in crud api also modify insert

--- application/controllers/api/Crud.php
+++ application/controllers/api/Crud.php
@@ -29,22 +29,41 @@ class Crud extends \Restserver\Libraries\REST_Controller
 
   function index_post()
   {
+    # NOTE: This is an example usage of batch upload
+    // $data = array_merge($this->input->post(), $this->crud_model->batch_upload($_FILES['input_name']));
 
-    if($last_id = $this->crud_model->add($this->input->post())){ # try to add and get the last id
-      $res = $this->crud_model->get($last_id); # get the last entry data
-      $this->response_header('Location', api_url($this) .  $last_id); # set the header location
+    $data = $this->input->post();
+
+    if($last_id = $this->crud_model->add($data)){ # Try to add and get the last id
+      $res = $this->crud_model->get($last_id); # Get the last entry data
+      $this->response_header('Location', api_url($this) .  $last_id); # Set the header location
       $this->response($res, 201);
     }else{
       $this->response(['message' => 'Malformed syntax'], 400);
     }
+  }
 
+  /**
+  * edit single
+  * @param  int $id [description]
+  */
+  function single_post($id)
+  {
+    $res = $this->crud_model->update($id, $this->input->post());
+    if($res){
+      $res = $this->crud_model->get($id);
+      $this->response_header('Location', api_url($this) .  $id); # Set the newly created object's location
+      $this->response($res, 200);
+    }else{
+      $this->response(['message' => 'Malformed syntax'], 400);
+    }
   }
 
   function single_delete($id)
   {
     $res = $this->crud_model->delete($id);
     if($res > 0){
-      $this->response($res, 204);  # omits the response anyway if 204
+      $this->response($res, 204); # Omits the response anyway if 204
     }else{
       $this->response(['message' => 'Not found'], 404 );
     }
