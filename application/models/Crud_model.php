af4a121bb58c7b2148c97c5d9c49d06880c6a1eb
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-06-16 16:27:46 +0800

    refactor upload model and add documentation

--- application/models/Crud_model.php
+++ application/models/Crud_model.php
@@ -2,8 +2,10 @@
 
 class Crud_model extends CI_model
 {
-
-  # Your table name here
+  /**
+   * Your table name
+   * @var string
+   */
   protected $table;
 
   public function __construct()
@@ -12,23 +14,42 @@ class Crud_model extends CI_model
     $this->table = 'crud';
   }
 
+  /**
+   * Get all rows from the table
+   * @return array
+   */
   public function all()
   {
     return $this->db->get($this->table)->result();
   }
 
+  /**
+   * Get specific row via id
+   * @param  int     $id
+   * @return array   associative array of data
+   */
   public function get($id)
   {
     $this->db->where('id', $id);
     return $this->db->get($this->table)->result();
   }
 
+  /**
+   * Inserts to the table with the associative array provided
+   * @param  array $data
+   * @return int   the last insert id
+   */
   public function add($data)
   {
     $this->db->insert($this->table, $data);
     return $this->db->insert_id();
   }
 
+ /**
+  * Deletes the row via id
+  * @param  int $id
+  * @return int number of rows deleted
+  */
   public function delete($id)
   {
     $this->db->where('id', $id);
@@ -36,42 +57,50 @@ class Crud_model extends CI_model
     return $this->db->affected_rows();
   }
 
+  /**
+   * Simply updates the table matching the associative array provided
+   * @param  int    $id    id of row to update
+   * @param  array  $data  associative array of values to be updated
+   * @return bool
+   */
   public function update($id, $data)
   {
     return $this->db->update($this->table, $data, ['id' => $id]);
   }
 
-  public function upload($files)
+  public function upload()
   {
-    $k = key($_FILES); # gets the key of the uplaoded thing on your form
-    $files = $files[$k]; # gets whatever the value of your uploaded thing is
+    # Defaults
+    $k = key($_FILES); # Gets the `key` of the uplaoded thing on your form
+    $files = @$_FILES[$k]; # Gets whatever the value of your uploaded thing is
+    $uploaded_files = []; # Initialize empty return array
+    $upload_path = 'uploads/your_dir'; # Your upload path starting from the `root folder`
 
-    $uploaded_files = [];
+    # Configs
+    $config['upload_path'] = $upload_path; # Set upload path
+    $config['allowed_types'] = 'gif|jpg|jpeg|png'; # Set allowed types
 
-    $upload_path = 'uploads/your_dir';
-
-    $config['upload_path'] = $upload_path;
-    $config['allowed_types'] = 'gif|jpg|jpeg|png';
-
-    if (!is_dir($upload_path) && !mkdir($upload_path, 0777, true)){
-      mkdir($upload_path, 0777, true);
+    # Folder creation
+    if (!is_dir($upload_path) && !mkdir($upload_path, DEFAULT_FOLDER_PERMISSIONS, true)){
+      mkdir($upload_path, DEFAULT_FOLDER_PERMISSIONS, true); # You can set DEFAULT_FOLDER_PERMISSIONS constant in application/config/constants.php
     }
 
-    foreach ($files['name'] as $key => $image) {
+    if($files === null) return []; # Returns an empty array if the `$_FILES` array is empty
 
+    foreach ($files['name'] as $key => $image) {
       $_FILES[$k]['name'] = $files['name'][$key];
       $_FILES[$k]['type'] = $files['type'][$key];
       $_FILES[$k]['tmp_name'] = $files['tmp_name'][$key];
       $_FILES[$k]['error'] = $files['error'][$key];
       $_FILES[$k]['size'] = $files['size'][$key];
 
-      $filename = time() . "_" . $files['name'][$key];
-      $images[] = $uploaded_files[] = $filename;
+      $filename = time() . "_" . $files['name'][$key]; # Renames the filename into timestamp_filename
+      $images[] = $uploaded_files[$k][] = $filename; # Appends all filenames to our return array with the key
 
       $config['file_name'] = $filename;
       $this->upload->initialize($config);
 
-      $this->upload->do_upload('images');
+      $this->upload->do_upload($k);
     }
 
     return $uploaded_files;
