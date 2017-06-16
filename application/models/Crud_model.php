afecd1142f3340c949d925ff8f19bb60cbeec3ad
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-06-16 18:13:12 +0800

    add uploading of single file

--- application/models/Crud_model.php
+++ application/models/Crud_model.php
@@ -112,17 +112,31 @@ class Crud_model extends CI_model
     return $uploaded_files;
   }
 
-  public function upload($file)
+  /**
+   * Upload single file. Returns an empty array on failure
+   * @param  string    $file_key   [description]
+   * @return array                 [description]
+   */
+  public function upload($file_key)
   {
+    $file = $_FILES[$file_key];
+    $upload_path = "uploads/your_dir";
 
-    $config['upload_path'] = "uploads/your_dir/"; # NOTE: Change your directory as needed
-    $config['allowed_types'] = 'gif|jpg|jpeg|png'; # NOTE: Change file tyoes as needed
-    $config['file_name'] = time() . $file['name']; # Set the new filename
+    $config['upload_path'] = $upload_path; # NOTE: Change your directory as needed
+    $config['allowed_types'] = 'gif|jpg|jpeg|png'; # NOTE: Change file types as needed
+    $config['file_name'] = time() . '_' . $file['name']; # Set the new filename
     $this->upload->initialize($config);
 
-    var_dump($file); die();
-    if ($this->upload->do_upload('profile_picture')) {
-      return $this->upload->data('file_ext');
+    # Folder creation
+    # TODO: Will refactor this and integrate it to the core upload class
+    if (!is_dir($upload_path) && !mkdir($upload_path, DEFAULT_FOLDER_PERMISSIONS, true)){
+      mkdir($upload_path, DEFAULT_FOLDER_PERMISSIONS, true); # You can set DEFAULT_FOLDER_PERMISSIONS constant in application/config/constants.php
+    }
+
+    if($this->upload->do_upload($file_key)){
+      return [$file_key => $this->upload->data('file_name')];
+    }else{
+      return [];
     }
 
   }
