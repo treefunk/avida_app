fafaf1a4977e895641cee7b4690a1b22a75e31e4
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-06-16 10:06:28 +0800

    refactor crud plus delete

--- application/controllers/api/Crud.php
+++ application/controllers/api/Crud.php
@@ -8,34 +8,23 @@ class Crud extends \Restserver\Libraries\REST_Controller
   function __construct()
   {
     parent::__construct();
-
     $this->load->model('crud_model');
   }
 
   function index_get()
   {
     $res = $this->crud_model->all();
-    $this->response(
-      $res, 200
-    );
+    $this->response($res, 200);
   }
 
   function single_get($id)
   {
     $res = $this->crud_model->get($id);
-
     if($res){
-
-      $this->response(
-        $res, 200
-      );
-
+      $this->response($res, 200);
     }else{
-      $this->response(
-        array('message' => 'Not found'), 404
-      );
+      $this->response(['message' => 'Not found'], 404);
     }
-
   }
 
   function index_post()
@@ -44,13 +33,9 @@ class Crud extends \Restserver\Libraries\REST_Controller
     if($last_id = $this->crud_model->add($this->input->post())){ # try to add and get the last id
       $res = $this->crud_model->get($last_id); # get the last entry data
       $this->response_header('Location', api_url($this) .  $last_id); # set the header location
-      $this->response(
-        $res, 201
-      );
+      $this->response($res, 201);
     }else{
-      $this->response(
-        array('message' => 'Malformed syntax'), 400
-      );
+      $this->response(['message' => 'Malformed syntax'], 400);
     }
 
   }
@@ -58,10 +43,11 @@ class Crud extends \Restserver\Libraries\REST_Controller
   function single_delete($id)
   {
     $res = $this->crud_model->delete($id);
-    $this->response(
-      $res, 200
-    );
+    if($res > 0){
+      $this->response($res, 204);  # omits the response anyway if 204
+    }else{
+      $this->response(['message' => 'Not found'], 404 );
+    }
   }
-
 
 }
