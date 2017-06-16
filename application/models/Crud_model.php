060871b91754d9cc9eb4f6c3db34056dd248f50b
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-06-16 17:19:43 +0800

    refactor batch upload and add unfinished upload

--- application/models/Crud_model.php
+++ application/models/Crud_model.php
@@ -3,9 +3,9 @@
 class Crud_model extends CI_model
 {
   /**
-   * Your table name
-   * @var string
-   */
+  * Your table name
+  * @var string
+  */
   protected $table;
 
   public function __construct()
@@ -15,19 +15,19 @@ class Crud_model extends CI_model
   }
 
   /**
-   * Get all rows from the table
-   * @return array
-   */
+  * Get all rows from the table
+  * @return array
+  */
   public function all()
   {
     return $this->db->get($this->table)->result();
   }
 
   /**
-   * Get specific row via id
-   * @param  int     $id
-   * @return array   associative array of data
-   */
+  * Get specific row via id
+  * @param  int     $id
+  * @return array   associative array of data
+  */
   public function get($id)
   {
     $this->db->where('id', $id);
@@ -35,17 +35,17 @@ class Crud_model extends CI_model
   }
 
   /**
-   * Inserts to the table with the associative array provided
-   * @param  array $data
-   * @return int   the last insert id
-   */
+  * Inserts to the table with the associative array provided
+  * @param  array $data
+  * @return int   the last insert id
+  */
   public function add($data)
   {
     $this->db->insert($this->table, $data);
     return $this->db->insert_id();
   }
 
- /**
+  /**
   * Deletes the row via id
   * @param  int $id
   * @return int number of rows deleted
@@ -58,39 +58,40 @@ class Crud_model extends CI_model
   }
 
   /**
-   * Simply updates the table matching the associative array provided
-   * @param  int    $id    id of row to update
-   * @param  array  $data  associative array of values to be updated
-   * @return bool
-   */
+  * Simply updates the table matching the associative array provided
+  * @param  int    $id    id of row to update
+  * @param  array  $data  associative array of values to be updated
+  * @return bool
+  */
   public function update($id, $data)
   {
     return $this->db->update($this->table, $data, ['id' => $id]);
   }
 
-/**
- * Batch upload of the given $_FILES[key] multiple upload input
- * @param  array   $files   example value is $_FILES[key]
- * @return array           returns a multidimensional array in this structure array[key] => [0 => 'file1', 1 => 'file2']
- */
+  /**
+  * Batch upload of the given $_FILES[key] multiple upload input
+  * @param  array   $files   example value is $_FILES[key]
+  * @return array           returns a multidimensional array in this structure array[key] => [0 => 'file1', 1 => 'file2']
+  */
   public function batch_upload($files = [])
   {
+
+    if($files == [] || $files == null ) return []; # Immediately returns an empty array if a parameter is not provided or key is not existing with the help of @ operator. Example @$_FILES['nonexistent_key']
+
     # Defaults
     $k = key($files); # Gets the `key` of the uplaoded thing on your form
 
     $uploaded_files = []; # Initialize empty return array
-    $upload_path = 'uploads/your_dir'; # Your upload path starting from the `root folder`
+    $upload_path = 'uploads/your_dir'; # Your upload path starting from the `root folder`. NOTE: Change this as needed
 
     # Configs
     $config['upload_path'] = $upload_path; # Set upload path
-    $config['allowed_types'] = 'gif|jpg|jpeg|png'; # Set allowed types
+    $config['allowed_types'] = 'gif|jpg|jpeg|png'; # NOTE: Change this as needed
 
     # Folder creation
     if (!is_dir($upload_path) && !mkdir($upload_path, DEFAULT_FOLDER_PERMISSIONS, true)){
       mkdir($upload_path, DEFAULT_FOLDER_PERMISSIONS, true); # You can set DEFAULT_FOLDER_PERMISSIONS constant in application/config/constants.php
     }
-
-    if($files === null) return []; # Returns an empty array if the `$_FILES` array is empty
 
     foreach ($files['name'] as $key => $image) {
       $_FILES[$k]['name'] = $files['name'][$key];
@@ -109,6 +110,21 @@ class Crud_model extends CI_model
     }
 
     return $uploaded_files;
+  }
+
+  public function upload($file)
+  {
+
+    $config['upload_path'] = "uploads/your_dir/"; # NOTE: Change your directory as needed
+    $config['allowed_types'] = 'gif|jpg|jpeg|png'; # NOTE: Change file tyoes as needed
+    $config['file_name'] = time() . $file['name']; # Set the new filename
+    $this->upload->initialize($config);
+
+    var_dump($file); die();
+    if ($this->upload->do_upload('profile_picture')) {
+      return $this->upload->data('file_ext');
+    }
+
   }
 
 }
